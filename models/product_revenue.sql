8b575de57cc3d6163e007c318a37686c189ef913
Sri Lalitha <anushaa1002@gmail.com>
2026-02-19 01:58:57 +0000

    commit changes

--- models/product_revenue.sql
+++ models/product_revenue.sql
@@ -1,6 +1,6 @@
 {{config(materialized='view')}}
 
  Select p.PRODUCTNAME, sum(ROUNDED_PRICE*o.quantity) as revenue from 
-    CURATED_DATA.DIM_PRODUCTS as p inner join
-    CURATED_DATA.FACT_ORDERS as o on p.PRODUCTID =o.PRODID
+    {{ref("dim_products")}} as p inner join
+    {{ref("fact_orders")}} as o on p.PRODUCTID =o.PRODID
     group by  p.PRODUCTNAME
---
+++ tests/emp_negative_check.sql
@@ -0,0 +1,2 @@
+select * from  {{ref("emp_less_avg")}}
+where salary <0
